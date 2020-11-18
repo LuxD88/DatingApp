f3395fa340fe2e1575d878613fa8831e722c8a21
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-11-18 18:22:37 +0100

    set JSON response to lowercase userName

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,8 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace API.DTOs
 {
     public class UserDto
     {
+        // [JsonName("birthdate")]
+        [JsonPropertyName("username")]
         public string UserName { get; set; }
+        [JsonPropertyName("token")]
         public string Token { get; set; }
 
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -20,6 +20,8 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using API.Extensions;
 using API.Middleware;
+// using Newtonsoft.Json.Serialization;
+// using System.Text.Json;
 
 namespace API
 {
@@ -42,6 +44,14 @@ namespace API
             services.AddControllers();
             services.AddCors();
             services.AddIdentityServices(_config);
+            // services.AddMvc(setupAction=> {
+            // setupAction.EnableEndpointRouting = false;
+            // }).AddJsonOptions(jsonOptions =>
+            //     {
+            //         jsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
+            //     });
+
+                // .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
