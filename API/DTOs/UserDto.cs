b5cc16b2eb84a484b0df15837ae0581d5e32083e
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-12-06 19:02:06 +0100

    148. Updating the API register method

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -10,5 +10,6 @@ namespace API.DTOs
         [JsonPropertyName("token")]
         public string Token { get; set; }
         public string PhotoUrl { get; set; }
+        public string KnownAs { get; set; }
     }
 }
