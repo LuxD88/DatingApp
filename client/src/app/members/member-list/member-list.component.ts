c91d635be48f94cb53a13ed524a59a776c21e5d0
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-12-08 14:26:15 +0100

    160. adding filter buttons to the client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -19,6 +19,7 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList = [{value: 'male', dispaly: 'Males'}, {value: 'female', dispaly: 'Females'}];
 
   constructor(private memberService: MembersService, private accountService: AccountService) { 
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
@@ -37,6 +38,11 @@ export class MemberListComponent implements OnInit {
       this.members = response.result;
       this.pagination = response.pagination;
     })
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.user);
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
