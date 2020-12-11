ccf67cf474655c3023632aa9dfeecefd6e2c2dcd
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-12-11 09:05:13 +0100

    183. Setting up the automapper profiles

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,6 +14,9 @@ namespace API.Helpers
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
+            CreateMap<Message, MessageDto>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => src.Sender.Photo.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src => src.Sender.Photo.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
