9b34fcd3099c8910e5c944259331a5fe5b31abeb
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-12-15 13:10:08 +0100

    204. Update the seed method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
         {
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
@@ -26,10 +25,8 @@ namespace API.Data
                 // user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("pass"));
                 // user.PasswordSalt = hmac.Key;
             
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
-
-            await context.SaveChangesAsync();
         }
     }
 }
