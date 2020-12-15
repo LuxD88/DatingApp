bdd5962aae9f67ade2c2ca211da2f78336e4b494
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-12-15 16:21:12 +0100

    207. Adding the rome to the JWT token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,10 +46,14 @@ namespace API.Controllers
 
             if (!result.Succeeded) return BadRequest(result.Errors);
 
+            var role = await _userManager.AddToRoleAsync(user, "Member");
+
+            if (!role.Succeeded) return BadRequest(role.Errors);
+
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -82,7 +86,7 @@ namespace API.Controllers
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
