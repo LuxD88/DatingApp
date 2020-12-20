766198c0623cac04102fefdc228a9fe645f70e49
Dobrev Yordan <Yordan.Dobrev@hannover-re.com>
2020-12-20 12:07:52 +0100

    229. Sending messages vie the hub

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
@@ -34,6 +35,12 @@ export class MessageService {
       this.hubConnection.on('ReceiveMessageThread', messages => {
         this.messageThreadSource.next(messages);
       })
+
+      this.hubConnection.on('NewMessage', message => {
+        this.messageThread$.pipe(take(1)).subscribe(messages => {
+          this.messageThreadSource.next([...messages, message])
+        })
+      })
   }
 
   stopHubConnection() {
@@ -55,8 +62,10 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage (username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', { recipientUsername: username, content}) // becaseu conent in the createMessageDto == content in the client, we don't have to say content: content and can just pass the content by yourself
+  async sendMessage (username: string, content: string) {
+    // return this.http.post<Message>(this.baseUrl + 'messages', { recipientUsername: username, content}) // becaseu conent in the createMessageDto == content in the client, we don't have to say content: content and can just pass the content by yourself
+    return this.hubConnection.invoke('SendMessage', { recipientUsername: username, content})
+       .catch(error => console.log(error));
   }
 
   deleteMessage (id: number) {
